3dab7d8e8fab94fce02af2069407a05ac8bb6c26
Ankit Roy <roy.ankit1602@gmail.com>
2021-04-10 22:39:51 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,17 +3,50 @@ import Loading from './Loading'
 import Tours from './Tours'
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-  const [loading,setLoading]
+  const [loading,setLoading]=useState(true)
   const [tours,setTours]=useState([])
+
+  const removeTour=(id)=>{
+    const newTours=tours.filter((tour)=>{
+      return tour.id!==id
+    })
+    setTours(newTours);
+  }
+
+  const fetchLoading=async ()=>{
+    try {
+      const response=await fetch(url)
+      const tours=await response.json()
+      setTours(tours)
+      setLoading(false)
+    } catch (error) {
+      setLoading(false)
+      console.log(error)
+    }
+    
+  }
+
+  useEffect(()=>{
+    fetchLoading()
+  },[])
+
   if(loading){
     return<main>
       <Loading />
     </main>
   }
-  useEffect(()=>{
-
-  },[])
-  return <h2>Tours Project Setup</h2>
+  if(tours.length ===0){
+    return <main>
+      <div className="title">
+        <h2>no tours left</h2>
+        <button className="btn" onClick={fetchLoading}>refresh</button>
+      </div>
+    </main>
+  }
+ 
+  return <main>
+    <Tours tours={tours} removeTour={removeTour}/>
+  </main>
 }
 
 export default App
